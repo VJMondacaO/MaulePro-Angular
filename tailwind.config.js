de6bffa1be7f7e22da867f03da953c4776952e7a
Víctor <131877086+VJMondacaO@users.noreply.github.com>
2025-12-09 10:49:27 -0300

    mejora breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        'md-large': '1360px', // Breakpoint para 2 columnas en pantallas medianas
+        'xl-large': '1700px', // Breakpoint para 4 columnas en pantallas grandes
         '3xl': '1920px', // Breakpoint personalizado para pantallas muy grandes
       },
       fontFamily: {
